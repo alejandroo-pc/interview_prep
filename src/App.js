b27080d75f91ce3af2207260b578d5e2f590d5bc
alejandro-patino-camargo <alejandropatinoc@gmail.com>
2024-01-06 20:59:28 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  /*
+1. create a simple todo app (using react state)
+2. render a list of data from an array of objects, passed from props
+3. find first non-repeating character in a string
+4. return number of vouwels in a string
+5. find two numbers from an array to add to a specific number
+*/
+
+  //  state for an array of words phrases as an array
+  //  state for the input value
+  //  function to handle the input value
+  //  function to add the input value to the array of words
+  //  function to delete a word from the array of words
+
+  const [inputTask, setInputTask] = useState("");
+  const [list, setList] = useState([]);
+
+  function handleInput(e) {
+    e.preventDefault();
+    setInputTask(e.target.value);
+  }
+
+  function handleAddTodo(todo) {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+
+    setList([...list, newTodo]);
+    setInputTask("");
+  }
+
+  function deleteTodo(id) {
+    const newTodo = list.filter((todo) => todo.id !== id);
+    setList(newTodo);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1> Todo List</h1>
+      <input
+        value={inputTask}
+        onChange={handleInput}
+        placeholder="enter a task"
+        type="text"
+      ></input>
+      <button onClick={() => handleAddTodo(inputTask)}> add</button>
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id}>
+            {todo.todo}
+            <button onClick={() => deleteTodo(todo.id)}>delete</button>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }
-
-export default App;
